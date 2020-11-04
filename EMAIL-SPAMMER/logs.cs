797502e63f50378995f34cbabe4d6010d77d8525
Jakub Sucheta <34921955+suchy2020@users.noreply.github.com>
2020-11-04 16:21:38 +0100

    #01 logs
    i do this better u know

---
+++ EMAIL-SPAMMER/logs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMAIL_SPAMMER
+{
+    public static class logs
+    {
+        public static void CreateFile()
+        {
+            if (!File.Exists("log.txt")) 
+                File.Create("log.txt");
+            AddLog($"[{DateTime.Now}] SPAMMER Started");
+        }
+
+        public static void AddLog(string x)
+        {
+            File.AppendAllText("log.txt", x);
+        }
+    }
+}
